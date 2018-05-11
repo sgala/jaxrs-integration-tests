6225a2a7d49af878bba29b3b51e5a16e21bcb29b
ivo <jsannn@gmail.com>
2018-05-11 10:43:39 +0300

    add mbw providers tests

---
+++ src/test/java/com/redhat/providers/mbw/resource/MessageBodyWriterObjectMessage.java
@@ -0,0 +1,9 @@
+package com.redhat.providers.mbw.resource;
+
+public class MessageBodyWriterObjectMessage {
+   
+   private String s;
+   public MessageBodyWriterObjectMessage(String s) { this.s = s; }
+   public String getS() { return s; }
+   public void setS(String s) { this.s = s; }
+}
